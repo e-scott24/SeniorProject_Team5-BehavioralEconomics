5c11837039389cc2ad150a68afbfb6b39ba562ad
e-scott24 <evanscott744@gmail.com>
2026-03-01 21:13:27 -0600

    Added 5 basic, foundational pages for the project (CreateSession, JoinSession, Lobby, Round1Career, and GameChanger). These will be heavily changed and modified as time goes on

---
+++ DealtHands/DealtHands/Pages/CreateSession.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DealtHands.Pages
+{
+    public class CreateSessionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DealtHands/DealtHands/Pages/GameChanger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DealtHands.Pages
+{
+    public class GameChangerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
