fb761f7e578e495df6fefa55851f9e7e1e43baf7
e-scott24 <evanscott744@gmail.com>
2026-03-01 22:08:09 -0600

    Added additional pages. Current pages include CreateSession, JoinSession, Lobby, GameChanger, Results, Rounds1-5. CreateSession page has basic functionality of selecting options and generating a code; it redirects to lobby page with code shown.

--- DealtHands/DealtHands/Pages/Lobby.cshtml.cs
+++ DealtHands/DealtHands/Pages/Lobby.cshtml.cs
@@ -5,8 +5,22 @@ namespace DealtHands.Pages
 {
     public class LobbyModel : PageModel
     {
-        public void OnGet()
+        public string SessionCode { get; set; }
+        public string SessionName { get; set; }
+        public string GameMode { get; set; }
+        public string Difficulty { get; set; }
+        public int MaxPlayers { get; set; }
+
+        public void OnGet(string sessionCode)
         {
+            // Receive session code from URL parameter
+            SessionCode = sessionCode;
+
+            // Get other session details from TempData
+            SessionName = TempData["SessionName"]?.ToString() ?? "Unknown Session";
+            GameMode = TempData["GameMode"]?.ToString() ?? "Random";
+            Difficulty = TempData["Difficulty"]?.ToString() ?? "Medium";
+            MaxPlayers = TempData["MaxPlayers"] != null ? (int)TempData["MaxPlayers"] : 35;
         }
     }
 }
