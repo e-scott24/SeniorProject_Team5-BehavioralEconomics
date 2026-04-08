507aafc7494119b876d9f8db24e21e1486b3d5df
Jason <jablack@usi.eagles.edu>
2026-04-08 15:22:28 -0500

    Added database

--- DealtHands/DealtHands/Migrations/20260408200759_GameChanger_Modifications.Designer.cs
+++ DealtHands/DealtHands/Migrations/20260408200759_GameChanger_Modifications.Designer.cs
@@ -4,6 +4,7 @@ using DealtHands.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DealtHands.Migrations
 {
     [DbContext(typeof(DealtHandsDbv2Context))]
-    partial class DealtHandsDbv2ContextModelSnapshot : ModelSnapshot
+    [Migration("20260408200759_GameChanger_Modifications")]
+    partial class GameChanger_Modifications
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
